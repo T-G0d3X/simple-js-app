3a1417431535792994349759ff8f568cb518f08e
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2020-11-23 22:47:26 +0100

    If applied this commit adds pokemon array
    pokemonList array with 4 objects containing info on pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert("Hello World");
-
-const favoriteFood = "pizza";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Venusaur', height: 5, type: ['grass', 'poison'] },
+  { name: 'Charmeleon', height: 8, type: ['fire'] },
+  { name: 'Blastoise', height: 9, type: ['water'] },
+  { name: 'Pikachu', height: 4, type: ['electric'] },
+];
