2a10d164480665982a45e03de652726afe4e232c
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2020-11-24 20:27:05 +0100

    If applied this commit adds loop
    This loop, loops over pokemonList array and adds it to the page with document.write

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 let pokemonList = [
-  { name: 'Venusaur', height: 5, type: ['grass', 'poison'] },
-  { name: 'Charmeleon', height: 8, type: ['fire'] },
-  { name: 'Blastoise', height: 9, type: ['water'] },
+  { name: 'Venusaur', height: 3, type: ['grass', 'poison'] },
+  { name: 'Charmeleon', height: 5, type: ['fire'] },
   { name: 'Pikachu', height: 4, type: ['electric'] },
+  { name: 'Blastoise', height: 9, type: ['water'] },
 ];
+
+//  loop over pokemonList array, writing name and height
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(
+    `${pokemonList[i].name} has a height of ${pokemonList[i].height} <br> `
+  );
+
+  if (pokemonList[i].height > 5) {
+    document.write(` - Wow, that's big!`);
+  }
+}
