83060242a374cb34af1c71f9d7d0004a542354e8
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2020-11-25 14:49:03 +0100

    This commit corrects the loop
    Now loop correctly writes the additional sentencte for big height

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,13 @@ let pokemonList = [
 
 //  loop over pokemonList array, writing name and height
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(
-    `${pokemonList[i].name} has a height of ${pokemonList[i].height} <br> `
-  );
-
   if (pokemonList[i].height > 5) {
-    document.write(` - Wow, that's big!`);
+    document.write(
+      `${pokemonList[i].name} has a height of ${pokemonList[i].height} - Wow, that's big!<br>`
+    );
+  } else {
+    document.write(
+      `${pokemonList[i].name} has a height of ${pokemonList[i].height} <br> `
+    );
   }
 }
