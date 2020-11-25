cb57bc2c11011a04567918d148b8b4a3f8d5279f
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2020-11-25 20:10:17 +0100

    If applied this commit adds forEach() loop, pokemonList wrapped n IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,59 @@
-let pokemonList = [
-  { name: 'Venusaur', height: 3, type: ['grass', 'poison'] },
-  { name: 'Charmeleon', height: 5, type: ['fire'] },
-  { name: 'Pikachu', height: 4, type: ['electric'] },
-  { name: 'Blastoise', height: 9, type: ['water'] },
-];
+// added
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: 'Venusaur', height: 3, type: ['grass', 'poison'] },
+    { name: 'Charmeleon', height: 5, type: ['fire'] },
+    { name: 'Pikachu', height: 4, type: ['electric'] },
+    { name: 'Blastoise', height: 9, type: ['water'] },
+  ];
 
-//  loop over pokemonList array, writing name and height
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 5) {
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(poke) {
+    // here we say typeof argument we pass into add function needs to be object for new pokemon
+    if (typeof poke === 'object') {
+      pokemonList.push(poke);
+    } else {
+      console.log('Invalid input');
+    }
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+// Here is value saved to variable pokemons --> pokemonList array
+let pokemons = pokemonRepository.getAll();
+// console.log(pokemons);
+
+// loop using .forEach that writes strings to the page
+pokemons.forEach(function (pokemon) {
+  if (pokemon.height > 5) {
     document.write(
-      `${pokemonList[i].name} has a height of ${pokemonList[i].height} - Wow, that's big!<br>`
+      `${pokemon.name} has a height of ${pokemon.height} - Wow, that's big!<br>`
     );
   } else {
-    document.write(
-      `${pokemonList[i].name} has a height of ${pokemonList[i].height} <br> `
-    );
+    document.write(`${pokemon.name} has a height of ${pokemon.height} <br> `);
   }
-}
+});
+
+// adding one more pokemon to the list
+pokemonRepository.add({ name: 'Marowak', height: 4.5, type: ['ground'] });
+console.log(pokemons);
+
+//  loop over pokemonList array, writing name and height
+// for (let i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].height > 5) {
+//     document.write(
+//       `${pokemonList[i].name} has a height of ${pokemonList[i].height} - Wow, that's big!<br>`
+//     );
+//   } else {
+//     document.write(
+//       `${pokemonList[i].name} has a height of ${pokemonList[i].height} <br> `
+//     );
+//   }
+// }
