cbb7f122afbd0d63ba805b7fbecb3031c50b6881
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2020-11-26 19:21:25 +0100

    If applied this commit adds new logic to app

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// added
+// added IIFE- immediately invoked function expression
 let pokemonRepository = (function () {
   let pokemonList = [
     { name: 'Venusaur', height: 3, type: ['grass', 'poison'] },
@@ -13,47 +13,68 @@ let pokemonRepository = (function () {
 
   function add(poke) {
     // here we say typeof argument we pass into add function needs to be object for new pokemon
-    if (typeof poke === 'object') {
+    if (typeof poke === 'object' && hasProperties(poke)) {
       pokemonList.push(poke);
     } else {
       console.log('Invalid input');
     }
   }
 
+  function hasProperties(pokemon) {
+    return (
+      pokemon.hasOwnProperty('name') &&
+      pokemon.hasOwnProperty('height') &&
+      pokemon.hasOwnProperty('type')
+    );
+  }
+
+  function getPokemon(name) {
+    return pokemonList.filter((pokemon) => pokemon.name === name);
+  }
+
+  function addListItem(pokemon) {
+    // variables for slecection of elements
+    let pokeList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    // text inside button
+    button.innerText = pokemon.name;
+    // adding elements
+    button.classList.add('btn');
+    listItem.appendChild(button);
+    pokeList.appendChild(listItem);
+    // event on a click that shows pokemon name in console
+    button.addEventListener('click', function () {
+      // logs details in console
+      showDetails(pokemon);
+    });
+  }
+  // function that logs pokemon info
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
   return {
     getAll: getAll,
     add: add,
+    hasProperties: hasProperties,
+    getPokemon: getPokemon,
+    addListItem: addListItem,
+    showDetails: showDetails,
   };
 })();
 
 // Here is value saved to variable pokemons --> pokemonList array
 let pokemons = pokemonRepository.getAll();
-// console.log(pokemons);
 
-// loop using .forEach that writes strings to the page
+// loop using .forEach that uses addListItem() function
 pokemons.forEach(function (pokemon) {
-  if (pokemon.height > 5) {
-    document.write(
-      `${pokemon.name} has a height of ${pokemon.height} - Wow, that's big!<br>`
-    );
-  } else {
-    document.write(`${pokemon.name} has a height of ${pokemon.height} <br> `);
-  }
+  pokemonRepository.addListItem(pokemon);
 });
 
 // adding one more pokemon to the list
 pokemonRepository.add({ name: 'Marowak', height: 4.5, type: ['ground'] });
-console.log(pokemons);
 
-//  loop over pokemonList array, writing name and height
-// for (let i = 0; i < pokemonList.length; i++) {
-//   if (pokemonList[i].height > 5) {
-//     document.write(
-//       `${pokemonList[i].name} has a height of ${pokemonList[i].height} - Wow, that's big!<br>`
-//     );
-//   } else {
-//     document.write(
-//       `${pokemonList[i].name} has a height of ${pokemonList[i].height} <br> `
-//     );
-//   }
-// }
+// calling pokemon with his name
+console.log(pokemonRepository.getPokemon('Venusaur'));
+console.log(pokemons);
