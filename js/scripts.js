1685ffff30e37a13e52ce0de0a13903c3edd363a
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2020-11-28 19:11:32 +0100

    If applied this commit adds external API for 150pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
+'use strict';
+
 // added IIFE- immediately invoked function expression
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: 'Venusaur', height: 3, type: ['grass', 'poison'] },
-    { name: 'Charmeleon', height: 5, type: ['fire'] },
-    { name: 'Pikachu', height: 4, type: ['electric'] },
-    { name: 'Blastoise', height: 9, type: ['water'] },
-  ];
-
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  //////////////////////////////////////
   function getAll() {
     return pokemonList;
   }
-
+  /////////////////////////////////////
   function add(poke) {
     // here we say typeof argument we pass into add function needs to be object for new pokemon
-    if (typeof poke === 'object' && hasProperties(poke)) {
+    if (typeof poke === 'object') {
       pokemonList.push(poke);
     } else {
       console.log('Invalid input');
     }
   }
-
+  ////////////////////////////////////
   function hasProperties(pokemon) {
     return (
       pokemon.hasOwnProperty('name') &&
@@ -27,11 +25,11 @@ let pokemonRepository = (function () {
       pokemon.hasOwnProperty('type')
     );
   }
-
+  ////////////////////////////////////
   function getPokemon(name) {
     return pokemonList.filter((pokemon) => pokemon.name === name);
   }
-
+  ////////////////////////////////////
   function addListItem(pokemon) {
     // variables for slecection of elements
     let pokeList = document.querySelector('.pokemon-list');
@@ -45,13 +43,64 @@ let pokemonRepository = (function () {
     pokeList.appendChild(listItem);
     // event on a click that shows pokemon name in console
     button.addEventListener('click', function () {
-      // logs details in console
       showDetails(pokemon);
     });
   }
-  // function that logs pokemon info
-  function showDetails(pokemon) {
-    console.log(pokemon.name);
+  ////////////////////////////////////
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+        hideLoadingMessage();
+      })
+      .catch(function (e) {
+        console.error(e);
+        hideLoadingMessage();
+      });
+  }
+  ////////////////////////////////////
+  function loadDetails(item) {
+    showLoadingMessage();
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        // add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+        hideLoadingMessage();
+      })
+      .catch(function (e) {
+        console.error(e);
+        hideLoadingMessage();
+      });
+  }
+  ////////////////////////////////////
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
+    });
+  }
+  ////////////////////////////////////
+  function showLoadingMessage() {
+    console.log('Loading');
+  }
+  ////////////////////////////////////
+  function hideLoadingMessage() {
+    console.log('⬇ Your pokemon is here ⬇');
   }
 
   return {
@@ -60,21 +109,68 @@ let pokemonRepository = (function () {
     hasProperties: hasProperties,
     getPokemon: getPokemon,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
     showDetails: showDetails,
   };
 })();
 
-// Here is value saved to variable pokemons --> pokemonList array
-let pokemons = pokemonRepository.getAll();
-
-// loop using .forEach that uses addListItem() function
-pokemons.forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  //  data is loaded!
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
 
-// adding one more pokemon to the list
-pokemonRepository.add({ name: 'Marowak', height: 4.5, type: ['ground'] });
+// // Here is value saved to variable pokemons --> pokemonList array
+// let pokemons = pokemonRepository.getAll();
 
-// calling pokemon with his name
-console.log(pokemonRepository.getPokemon('Venusaur'));
-console.log(pokemons);
+// // loop using .forEach that uses addListItem() function
+// pokemons.forEach(function (pokemon) {
+//   pokemonRepository.addListItem(pokemon);
+// });
+
+// // adding one more pokemon to the list
+// pokemonRepository.add({ name: 'Marowak', height: 4.5, type: ['ground'] });
+
+// // calling pokemon with his name
+// console.log(pokemonRepository.getPokemon('Venusaur'));
+// console.log(pokemons);
+
+//  Practice
+
+// // function that shows pokemon info under names
+
+// function showDetails(pokemon) {
+//   if (pokemon.name === 'Venusaur') {
+//     fillInfo.innerText = `${pokemon.name} > plants blooms when it is absorbing solar energy. It stays on the move to seek sunlight.
+//     Has a height of ${pokemon.height}, and its type is ${pokemon.type} has Overgrow ability`;
+//     fillInfo.style.color = 'white';
+//   } else if (pokemon.name === 'Charmeleon') {
+//     fillInfo.innerText = `${pokemon.name} > It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.
+//     Has a height of ${pokemon.height}, and its type is ${pokemon.type} has Burn ability`;
+//     fillInfo.style.color = 'white';
+//   } else if (pokemon.name === 'Pikachu') {
+//     fillInfo.innerText = `${pokemon.name} > can generate powerful electricity, have cheek sacs that are extra soft and super stretchy.
+//     Has a height of ${pokemon.height}, and its type is ${pokemon.type} has Static ability`;
+//     fillInfo.style.color = 'white';
+//   } else if (pokemon.name === 'Blastoise') {
+//     fillInfo.innerText = `${pokemon.name} > It crushes its foe under its heavy body to cause fainting.
+//     Has a height of ${pokemon.height}, and its type is ${pokemon.type} has Torrent ability`;
+//     fillInfo.style.color = 'white';
+//   } else {
+//   }
+// }
+
+// shows pokemon information when clicked on name buttons
+// function showPokeInfo() {
+//   const pokeInfo = document.querySelector('.pokemon-info-clicked');
+//   pokeInfo.style.display = 'block';
+// }
+
+// // closes pokemon information when escape is pressed
+// document.addEventListener('keydown', function (e) {
+//   if (e.key === 'Escape') {
+//     fillInfo.style.display = 'none';
+//   }
+// });
