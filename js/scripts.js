40fbff446294a3979dbf319168f9b3def009f5c4
Tihomir Godek <69514083+T-G0d3X@users.noreply.github.com>
2020-12-01 17:36:40 +0100

    If applied adds data from external API, click functions, modals, dialogs

--- js/scripts.js
+++ js/scripts.js
@@ -80,7 +80,15 @@ let pokemonRepository = (function () {
         // add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
-        item.types = details.types;
+        // function to loop over object and get ability name
+        function abilityName() {
+          const abilityVar = Object.entries(details.abilities[0]);
+          for (const [key, { name }] of abilityVar) {
+            abilityName = `mighty ability is >>${name}<<`;
+            return abilityName;
+          }
+        }
+        item.nameAbility = abilityName();
         hideLoadingMessage();
       })
       .catch(function (e) {
@@ -91,7 +99,11 @@ let pokemonRepository = (function () {
   ////////////////////////////////////
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
-      console.log(item);
+      showModal(
+        item.name,
+        `has height of: ${item.height} and ${item.nameAbility}`,
+        item.imageUrl
+      );
     });
   }
   ////////////////////////////////////
@@ -103,6 +115,123 @@ let pokemonRepository = (function () {
     console.log('⬇ Your pokemon is here ⬇');
   }
 
+  //////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
+  function showModal(title, text, img) {
+    let modalContainer = document.querySelector('#modal-container');
+
+    // Clears all existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    //Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'x';
+    // to close modal with button
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    // setting image in modal
+    let container = document.querySelector('#image-container');
+    let myImage = document.createElement('img');
+    myImage.src = img;
+
+    modal.appendChild(myImage);
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal(
+      'Pokeballs! Round n shiny',
+      'When the ball hits a Pokémon, they are converted into a form of energy, and are sent flying inside of it. Throw it and catch some pokemons!'
+    );
+  });
+
+  document.querySelector('#show-dialog').addEventListener('click', () => {
+    showDialog('Confirm action', 'Are you sure you want to do this?').then(
+      function () {
+        alert('confirmed!');
+      },
+      () => {
+        alert('not confirmed');
+      }
+    );
+  });
+
+  let dialogPromiseReject;
+  // function that we use to close modal on close button
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+
+    if (dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  });
+  /////////////////////////////////////////
+  /////////////////////////////////////////
+  function showDialog(title, text) {
+    showModal(title, text);
+
+    // defined modalContainer
+    let modalContainer = document.querySelector('#modal-container');
+
+    // adding confirm and cancel button to the modal
+    let modal = modalContainer.querySelector('.modal');
+
+    let confirmButton = document.createElement('button');
+    confirmButton.classList.add('modal-confirm');
+    confirmButton.innerText = 'Confirm';
+
+    let cancelButton = document.createElement('button');
+    cancelButton.classList.add('modal-cancel');
+    cancelButton.innerText = 'Cancel';
+
+    modal.appendChild(confirmButton);
+    modal.appendChild(cancelButton);
+
+    // focus confirmButton so user can press enter
+    confirmButton.focus();
+
+    return new Promise((resolve, reject) => {
+      cancelButton.addEventListener('click', hideModal);
+      confirmButton.addEventListener('click', () => {
+        dialogPromiseReject = null;
+        hideModal();
+        resolve();
+      });
+
+      dialogPromiseReject = reject;
+    });
+  }
+
+  ////////////////////////////////////
   return {
     getAll: getAll,
     add: add,
@@ -121,6 +250,79 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+(function () {
+  // form validation function
+  let form = document.querySelector('#register-form');
+  let emailInput = document.querySelector('#email');
+  let passwordInput = document.querySelector('#password');
+
+  function validateEmail() {
+    let value = emailInput.value;
+
+    if (!value) {
+      showErrorMessage(emailInput, 'Email is required field');
+      return false;
+    }
+
+    if (value.indexOf('@') === -1) {
+      showErrorMessage(emailInput, 'You must enter a valid e-mail address');
+      return false;
+    }
+
+    showErrorMessage(emailInput, null);
+    return true;
+  }
+
+  function validatePassword() {
+    let value = passwordInput.value;
+
+    if (!value) {
+      showErrorMessage(passwordInput, 'Password is a required field!');
+      return false;
+    }
+    if (value.length < 8) {
+      showErrorMessage(
+        passwordInput,
+        'The password needs to be at least 8 characters long'
+      );
+      return false;
+    }
+    showErrorMessage(passwordInput, null);
+    return true;
+  }
+
+  function validateForm() {
+    let isValidEmail = validateEmail();
+    let isValidPassword = validatePassword();
+    return isValidEmail && isValidPassword;
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      alert('Success!');
+    }
+  });
+
+  emailInput.addEventListener('input', validateEmail);
+  passwordInput.addEventListener('input', validatePassword);
+
+  function showErrorMessage(input, message) {
+    let container = input.parentElement;
+    let error = container.querySelector('.error-message');
+    if (error) {
+      container.removeChild(error);
+    }
+
+    if (message) {
+      let error = document.createElement('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  }
+})();
 
 // // Here is value saved to variable pokemons --> pokemonList array
 // let pokemons = pokemonRepository.getAll();
